6ff717f895868b52dc1c75b0ff82d6ece7ec823d
Strooom <pascal.roobrouck@gmail.com>
2021-02-10 11:32:02 +0100

    add first example

---
+++ examples/default/default.ino
@@ -0,0 +1,24 @@
+#include <STMPE811_SPI.h>
+
+#define chipSelectPin 25
+
+
+STMPE811 theTouchController(chipSelectPin);
+
+void setup() {
+    theTouchController.initialize();
+    Serial.begin(115200);
+    Serial.flush();
+    Serial.print("STMPE811 - Version ");
+    Serial.println(theTouchController.getVersion());
+}
+
+void loop() {
+    if (theTouchController.isTouched()) {
+        Serial.print("Touched : X =  ");
+        Serial.print(theTouchController.getX());
+        Serial.print(" Y =  ");
+        Serial.println(theTouchController.getY());
+        delay(1000);
+    }
+}
